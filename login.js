89d7e6a9e3c9688e4de33cb4027eb086bab508f4
nrankin96 <nykiarankin96@gmail.com>
2022-10-22 20:06:33 -0400

    create register

--- login.js
+++ login.js
@@ -10,18 +10,6 @@ const connection = mysql.createConnection({
     database: 'nodelogin'
 });
 
-connection.connect(function(err) {
-    if (err) {
-        console.log(err);
-    }
-    console.log("Connected!");
-    connection.query('CREATE DATABASE nodelogin', (err, result) => {
-        if (err){
-            console.log(err);
-        }
-        console.log('Database created!');
-    })
-  });
 
 const app = express();
 app.use(session({
@@ -32,18 +20,55 @@ app.use(session({
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'static')));
+app.use('/static', express.static(__dirname + '/static'));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/login'));
+    res.sendFile(path.join(__dirname + '/login.html'));
+
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname + '/register.html'));
 });
 
 app.post('/auth', (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
+    let email = req.body.email;
 
     if (username && password) {
-        
-    }
+        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+            if(err) {
+                throw err
+            };
+            if(results.length >0) {
+                req.session.loggedin = true;
+                req.session.username = username;
+                res.redirect('/home');
+            } else {
+                res.send('Incorrect Username and/or Password');
+            }
+            res.end();
+    });
+} else {
+    res.send('Please enter Username and Password.');
+    res.end();
+}
 });
+
+app.get('/home', (req, res) => {
+    if(req.session.loggedin) {
+        res.send('Welcome back ' + req.session.username + ' !');
+    } else {
+        res.send('Please login to view this page!');
+    }
+    res.end();
+});
+
+
+
+app.listen(3000, (req, res) => {
+    console.log('Servers started on port 3000');
+});
+    
